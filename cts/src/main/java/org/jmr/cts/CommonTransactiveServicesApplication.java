07d4ec770dd39da28d5ccd8eb0451451aec3cbda
jackr276 <jack_m_robbins@me.com>
2024-12-28 10:24:05 -0500

    Setup, first build running

---
+++ cts/src/main/java/org/jmr/cts/CommonTransactiveServicesApplication.java
@@ -0,0 +1,13 @@
+package org.jmr.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTransactiveServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonTransactiveServicesApplication.class, args);
+	}
+
+}
---
+++ cts/src/test/java/org/jmr/cts/CommonTransactiveServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jmr.cts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTransactiveServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
