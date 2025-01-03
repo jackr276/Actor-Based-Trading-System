5dff09d1cb2a8de021674e5653778db7a0ddaadb
jackr276 <jack_m_robbins@me.com>
2025-01-03 13:08:58 -0500

    Small changes to Local market agent

--- market/src/main/java/org/jmr/market/controllers/LocalMarketAgentRESTController.java
+++ market/src/main/java/org/jmr/market/controllers/LocalMarketAgentRESTController.java
@@ -1,6 +1,7 @@
 package org.jmr.market.controllers;
 
 import org.jmr.market.payloads.RegisterActorResponse;
+import org.jmr.market.util.ActorIdType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/local_market_agent")
 public class LocalMarketAgentRESTController {
+
+
+
+
+	/**
+	 * A private helper that will insert the actor into our known actors list
+	 */
+	private void registerActor(ActorIdType actorId){
+
+	}
+
 
 	@PostMapping("/registerActor")
 	public RegisterActorResponse registerActor(){
