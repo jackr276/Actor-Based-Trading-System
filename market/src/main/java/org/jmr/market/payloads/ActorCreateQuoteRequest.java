1d60707a29914899be1571b6d93674be1f27bd3c
jackr276 <jack_m_robbins@me.com>
2025-01-10 08:18:02 -0500

    Flesh out actor create quote payloads

--- market/src/main/java/org/jmr/market/payloads/ActorCreateQuoteRequest.java
+++ market/src/main/java/org/jmr/market/payloads/ActorCreateQuoteRequest.java
@@ -1,5 +1,79 @@
 package org.jmr.market.payloads;
 
-public class ActorCreateQuoteRequest{
+import java.time.Instant;
 
+import org.jmr.market.util.SideType;
+
+/**
+ * Actors will send this request to the market user agent, and the market user
+ * agent will handle the rest
+ */
+public class ActorCreateQuoteRequest{
+	private String info = "ActorCreateQuoteRequest";
+	//The instrument that we want to use
+	private long instrumentId;
+	//What side quote is it
+	SideType side;
+	//Is this a private quote
+	private boolean privateQuote;
+	//Offer price. Remember, all offer prices are divided by 10000
+	private long offerPrice;
+	//The expiration time
+	private Instant expirationTime;
+
+	//JSON
+	public ActorCreateQuoteRequest(){}
+	
+	public String getInfo(){
+		return this.info;
+	}
+
+	public void setInfo(String info){
+		this.info = info;
+	}
+
+	public long getInstrumentId(){
+		return this.instrumentId;
+	}
+
+	public void setInstrumentId(long instrumentId){
+		this.instrumentId = instrumentId;
+	}
+
+	public SideType getSide(){
+		return this.side;
+	}
+
+	public void setSide(SideType side){
+		this.side = side;
+	}
+
+	public boolean getPrivateQuote(){
+		return this.privateQuote;
+	}
+	
+	public void setPrivateQuote(boolean privateQuote){
+		this.privateQuote = privateQuote;
+	}
+
+	public long getOfferPrice(){
+		return this.offerPrice;
+	}
+
+	public void setOfferPrice(long offerPrice){
+		this.offerPrice = offerPrice;
+	}
+
+	public Instant getExpirationTime(){
+		return this.expirationTime;
+	}
+
+	public void setExpirationTime(Instant expirationTime){
+		this.expirationTime = expirationTime;
+	}
+
+	@Override
+	public String toString(){
+		return this.info + " " + this.instrumentId + " " + this.side + " " + this.expirationTime;
+	}
 }
--- market/src/main/java/org/jmr/market/payloads/ActorCreateQuoteResponse.java
+++ market/src/main/java/org/jmr/market/payloads/ActorCreateQuoteResponse.java
@@ -1,9 +1,11 @@
 package org.jmr.market.payloads;
 
+import org.jmr.market.quote.QuoteType;
 import org.jmr.market.util.ResponseType;
 
 public class ActorCreateQuoteResponse implements ActorResponse{
 	private String info = "ActorCreateQuoteResponse";
+	private QuoteType createdQuote;
 	private ResponseType response;
 
 	public ActorCreateQuoteResponse(){}
@@ -16,6 +18,14 @@ public class ActorCreateQuoteResponse implements ActorResponse{
 		this.info = info;
 	}
 
+	public QuoteType getCreatedQuote(){
+		return this.createdQuote;
+	}
+
+	public void setCreatedQuote(QuoteType createdQuote){
+		this.createdQuote = createdQuote;
+	}
+
 	public ResponseType getResponse(){
 		 return this.response;
 	}
@@ -26,6 +36,6 @@ public class ActorCreateQuoteResponse implements ActorResponse{
 
 	@Override
 	public String toString(){
-		return this.info + ": " + this.response;
+		return this.info + ": " + this.createdQuote + " " + this.response;
 	}
 }
